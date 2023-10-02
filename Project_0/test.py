a63e3c8b1732f8efc70294627b2f11c80549b71c
Elancinos <telischev.evgeny@gmail.com>
2023-10-02 20:03:53 +0300

    PY-10 added, PY-11 started

---
+++ Project_0/test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+# print(len(np.sctypeDict))
+# print(*sorted(map(str, set(np.sctypeDict.values()))), sep='\n')
+
+# a = np.float64(99999999)
+# print(a)
+
+# lin, step = np.linspace(-6, 21, 60, endpoint=False, retstep = True)
+# print(step)
+# print(np.iinfo(np.uint32))
+
+simplelist = [19, 242, 14, 152, 142, 1000]
+
+print(pd.__version__)
+print(pd.__name__)
